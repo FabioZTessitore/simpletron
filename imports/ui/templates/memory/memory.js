22f72959e46a600fad92b7897fe9b4249fbc6246
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-02-21 11:38:56 +0100

    memory fix

--- imports/ui/templates/memory/memory.js
+++ imports/ui/templates/memory/memory.js
@@ -13,16 +13,23 @@ Template.Memory.onRendered( function() {
   const container = $('#memory');
   let item;
   let col = 0;
+  let row = 0;
   const m = this.memory.get();
   for (let i = 0; i < m.size; i++) {
-    if (col==0 || col==11) {
+    if(col==11) {
       item = $('<div class="col s1">').text("_");
       container.append(item);
-    } else {
-      item = $('<div class="col s1">').text(m.m[i]);
-      container.append(item);
+      col = 0;
+      row++;
     }
+    if (col==0) {
+      item = $('<div class="col s1">').text(row);
+      container.append(item);
+      col++;
+    }
+
+    item = $('<div class="col s1">').text(m.m[i]);
+    container.append(item);
     col++;
-    if (col >= 12) col = 0;
   }
 });
